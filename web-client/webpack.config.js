0c66c06f6a63667da3ed320796f2abd42d795b8a
Dandan.Xue <tentenxue@gmail.com>
2021-05-31 17:49:21 +0200

    removed postcss from webpack

--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -36,21 +36,21 @@ module.exports = {
                   // translates CSS into CommonJS modules
                   loader: 'css-loader'
                 }, {
-                  // Run postcss actions
-                  loader: 'postcss-loader',
-                  options: {
-                    // `postcssOptions` is needed for postcss 8.x;
-                    // if you use postcss 7.x skip the key
-                    postcssOptions: {
-                      // postcss plugins, can be exported to postcss.config.js
-                      plugins: function () {
-                        return [
-                          require('autoprefixer')
-                        ];
-                      }
-                    }
-                  }
-                }, {
+                //   // Run postcss actions
+                //   loader: 'postcss-loader',
+                //   options: {
+                //     // `postcssOptions` is needed for postcss 8.x;
+                //     // if you use postcss 7.x skip the key
+                //     postcssOptions: {
+                //       // postcss plugins, can be exported to postcss.config.js
+                //       plugins: function () {
+                //         return [
+                //           require('autoprefixer')
+                //         ];
+                //       }
+                //     }
+                //   }
+                // }, {
                   // compiles Sass to CSS
                   loader: 'sass-loader'
                 }]
